87d20aa017f87289f0037eae35e664fbac8fb724
Gustavo Albuquerque <gustavoalbuquerquebr@gmail.com>
2019-10-08 12:47:00 -0300

    Updates

---
+++ precache-manifest.91cea048da98a0f8ae4b982093cd26e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a381d89da521e54bcb2db82a662e67",
+    "url": "/ghpages-demo/index.html"
+  },
+  {
+    "revision": "ba6eba482e9d403102d7",
+    "url": "/ghpages-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "91198175c0828c3657a2",
+    "url": "/ghpages-demo/static/js/2.ed182368.chunk.js"
+  },
+  {
+    "revision": "ba6eba482e9d403102d7",
+    "url": "/ghpages-demo/static/js/main.17e40da7.chunk.js"
+  },
+  {
+    "revision": "887288556eb49423bbba",
+    "url": "/ghpages-demo/static/js/runtime-main.4fe7870c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ghpages-demo/static/media/logo.25bf045c.svg"
+  }
+]);
